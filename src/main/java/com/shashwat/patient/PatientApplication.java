7b63e230dc362cfc63377d9c993c65d8254796eb
Shashwat Pathak <shashwatpathak18@gmail.com>
2022-08-29 01:25:23 -0400

    Update PatientApplication.java

--- src/main/java/com/shashwat/patient/PatientApplication.java
+++ src/main/java/com/shashwat/patient/PatientApplication.java
@@ -1,5 +1,7 @@
 package com.shashwat.patient;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -8,8 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PatientApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PatientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PatientApplication.class, args);
+    }
 }
